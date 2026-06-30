fe64a4eccaded283512d98278d385d9ea22e9ebb
Kaung Htet <kh622962@gmail.com>
2026-07-01 03:50:23 +0630

    chore(mail): publish and brand Laravel mail templates with MiraiStudy identity

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+<span style="font-size: 22px; font-weight: 800; color: #16A34A; letter-spacing: -0.5px;">Mirai</span><span style="font-size: 22px; font-weight: 800; color: #18181b; letter-spacing: -0.5px;">Study</span>
+</a>
+</td>
+</tr>
